c05ecbe2859d2dda769fe3078f24481da01955e2
r2road <studior2road@gmail.com>
2022-01-14 03:32:43 +0900

    update : test - visible_resource_research - DrawWithPosition

--- r2bix/src/research/visible_resource_research.cpp
+++ r2bix/src/research/visible_resource_research.cpp
@@ -26,22 +26,30 @@ namespace visible_resource_research
 			std::cout << r2::split;
 
 			const r2::VisibleResource visible_resource( 10, 10, '0' );
+			COORD pos = { 10, 11 };
 
-			HANDLE stdHandle = GetStdHandle( STD_OUTPUT_HANDLE );
-			COORD pos = { 10, 5 };
-			SetConsoleCursorPosition( stdHandle, pos );
+			std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
+			std::cout << r2::tab2 << "const r2::VisibleResource visible_resource( 10, 10, '0' );" << r2::linefeed;
+			std::cout << r2::tab2 << "COORD pos = { 10, 11 };" << r2::linefeed;
 
-			int count = 0;
-			for( const char element : visible_resource )
+			std::cout << r2::split;
+
 			{
-				std::cout << element;
+				HANDLE stdHandle = GetStdHandle( STD_OUTPUT_HANDLE );
+				SetConsoleCursorPosition( stdHandle, pos );
 
-				++count;
-				if( visible_resource.GetWidth() <= count )
+				int count = 0;
+				for( const char element : visible_resource )
 				{
-					count = 0;
-					pos.Y += 1;
-					SetConsoleCursorPosition( stdHandle, pos );
+					std::cout << element;
+
+					++count;
+					if( visible_resource.GetWidth() <= count )
+					{
+						count = 0;
+						pos.Y += 1;
+						SetConsoleCursorPosition( stdHandle, pos );
+					}
 				}
 			}
 
