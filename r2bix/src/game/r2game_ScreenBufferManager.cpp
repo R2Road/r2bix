d07580faf29e14c8dd0be19994e3030fc0e2aa35
r2road <studior2road@gmail.com>
2022-02-15 05:09:25 +0900

    rename

--- r2bix/src/game/r2game_ScreenBufferManager.cpp
+++ r2bix/src/game/r2game_ScreenBufferManager.cpp
@@ -20,8 +20,8 @@ namespace r2game
 		mBufferHandle4First = GetStdHandle( STD_OUTPUT_HANDLE );
 		assert( INVALID_HANDLE_VALUE != mBufferHandle4First );
 
-		CONSOLE_SCREEN_BUFFER_INFO first_csbi{};
-		assert( GetConsoleScreenBufferInfo( mBufferHandle4First, &first_csbi ) && "Failed : GetConsoleScreenBufferInfo" );
+		CONSOLE_SCREEN_BUFFER_INFO csbi{};
+		assert( GetConsoleScreenBufferInfo( mBufferHandle4First, &csbi ) && "Failed : GetConsoleScreenBufferInfo" );
 
 		mBufferHandle4Second = CreateConsoleScreenBuffer(
 			GENERIC_READ | GENERIC_WRITE
@@ -32,10 +32,10 @@ namespace r2game
 		);
 		assert( INVALID_HANDLE_VALUE != mBufferHandle4First );
 
-		SetConsoleScreenBufferSize( mBufferHandle4Second, first_csbi.dwSize );
+		SetConsoleScreenBufferSize( mBufferHandle4Second, csbi.dwSize );
 		{
 			DWORD out_result;
-			FillConsoleOutputCharacter( mBufferHandle4Second, TEXT( '2' ), first_csbi.dwSize.X * first_csbi.dwSize.Y, { 0, 0 }, &out_result );
+			FillConsoleOutputCharacter( mBufferHandle4Second, TEXT( '2' ), csbi.dwSize.X * csbi.dwSize.Y, { 0, 0 }, &out_result );
 		}
 	}
 
