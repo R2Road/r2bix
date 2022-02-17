72098519cb49934a5bdc01fe4c44a6d406ee3dd2
r2road <studior2road@gmail.com>
2022-02-17 10:21:13 +0900

    update : test - renderable_test - Basic

--- r2bix/src/test/renderable_test.cpp
+++ r2bix/src/test/renderable_test.cpp
@@ -3,8 +3,22 @@
 
 #include "base/r2base_eTestEndAction.h"
 
+#include "render/r2_iRenderable.h"
+#include "render/r2_VisibleResource.h"
+
 namespace renderable_test
 {
+	class RenderableObject : r2::iRenderable
+	{
+	public:
+		RenderableObject() : mVR( 10, 10, 'R' )
+		{}
+
+		void Render( const r2::Camera* const camera ) override {}
+
+		r2::VisibleResource mVR;
+	};
+
 	r2base::iTest::TitleFunc Basic::GetTitleFunction() const
 	{
 		return []()->const char*
@@ -18,11 +32,13 @@ namespace renderable_test
 		{
 			std::cout << "# " << GetInstance().GetTitleFunction()( ) << " #" << r2::linefeed;
 
+			RenderableObject ro;
+
 			std::cout << r2::split;
 
 			{
-				//std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
-				//std::cout << r2::tab2 << "r2::Camera camera( { 20, 30 }, { 20, 10 } );" << r2::linefeed;
+				std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
+				std::cout << r2::tab2 << "RenderableObject ro;" << r2::linefeed;
 			}
 
 			std::cout << r2::split;
