65d55ee3f4ea17cac5393f47a661212822876341
r2road <studior2road@gmail.com>
2022-03-04 01:55:27 +0900

    update : test - rect_test - Basic

--- r2bix/src/test/item/rect_test.cpp
+++ r2bix/src/test/item/rect_test.cpp
@@ -42,25 +42,24 @@ namespace rect_test
 			const r2::RectInt rect_0( 0, 0, 1, 1 );
 			const r2::RectInt rect_1( 4, 4, 6, 6 );
 			const r2::RectInt rect_2( 6, 6, 6, 6 );
+			const auto rect_3 = rect_1.IntersectsWithRect( rect_2 );
 
 			{
 				std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
 				std::cout << r2::tab2 << "const r2::RectInt rect_0( 0, 0, 1, 1 );" << r2::linefeed;
 				std::cout << r2::tab2 << "const r2::RectInt rect_1( 4, 4, 6, 6 );" << r2::linefeed;
 				std::cout << r2::tab2 << "const r2::RectInt rect_2( 6, 6, 6, 6 );" << r2::linefeed;
+				std::cout << r2::tab2 << "const auto rect_3 = rect_1.IntersectsWithRect( rect_2 );" << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				const int offset_y = 12;
+				const int offset_y = 13;
 
 				DrawRect( offset_y, rect_0, '0' );
 				DrawRect( offset_y, rect_1, '1' );
 				DrawRect( offset_y, rect_2, '2' );
-
-				const auto rect_3 = rect_1.IntersectsWithRect( rect_2 );
-
 				DrawRect( offset_y, rect_3, '3' );
 
 				SetConsoleCursorPosition(
