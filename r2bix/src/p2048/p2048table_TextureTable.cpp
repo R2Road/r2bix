84a2f86c0ab72584422a252c7bd56ce866740818
r2road <studior2road@gmail.com>
2022-03-04 00:07:28 +0900

    add class : p2048table - TextureTable

---
+++ r2bix/src/p2048/p2048table_TextureTable.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "p2048table_TextureTable.h"
+
+#include "render/r2render_Texture.h"
+#include "render/r2render_TextureFrame.h"
+
+#include "test/scene/DevelopScene.h"
+
+namespace p2048table
+{
+	r2render::TextureFrame* const TextureTable::GetTitleTexture() const
+	{
+		static r2render::Texture texture( 71, 9,
+			"#######################################################################"
+			"#                                                                     #"
+			"#      22222            00000            4  4             88888       #"
+			"#     2     2          0     0          4   4            8     8      #"
+			"#       2222           0     0          4   4             88888       #"
+			"#     22               0     0          4444444          8     8      #"
+			"#     2222222           00000               4             88888       #"
+			"#                                                                     #"
+			"#######################################################################"
+		);
+		static r2render::TextureFrame texture_frame( &texture );
+
+		return &texture_frame;
+	}
+}
---
+++ r2bix/src/p2048/p2048table_TextureTable.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "base/r2base_Singleton.h"
+
+namespace r2render
+{
+	class TextureFrame;
+}
+
+namespace p2048table
+{
+	class TextureTable : public r2base::SingleTon<TextureTable>
+	{
+	public:
+		r2render::TextureFrame* const GetTitleTexture() const;
+	};
+}
