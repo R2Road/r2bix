6d3c678afbf7cdf43d1152f00c98bf80e8dc8275
r2road <studior2road@gmail.com>
2022-03-10 22:10:56 +0900

    miss... o_o;;;;

--- r2bix/src/node/r2node_LabelNode.cpp
+++ r2bix/src/node/r2node_LabelNode.cpp
@@ -10,7 +10,7 @@ namespace r2node
 	std::unique_ptr<r2base::Node> LabelNode::Create( r2base::Director& director )
 	{
 		auto ret( r2base::Node::Create( director ) );
-		if( !ret )
+		if( ret )
 		{
 			r2component::TextureRenderComponent* texture_render_component = ret->AddComponent<r2component::TextureRenderComponent>();
 			r2component::LabelComponent* label_component = ret->AddComponent<r2component::LabelComponent>();
