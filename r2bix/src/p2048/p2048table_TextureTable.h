8320d3cd55c40a4709c98fb54be6821f057e6d60
r2road <studior2road@gmail.com>
2022-03-12 15:28:26 +0900

    split

--- r2bix/src/p2048/p2048table_TextureTable.h
+++ r2bix/src/p2048/p2048table_TextureTable.h
@@ -5,7 +5,7 @@
 #include <unordered_map>
 
 #include "r2/r2_Singleton.h"
-#include "render/r2render_TextureFrame.h"
+#include "base/r2base_TextureTableBase.h"
 
 namespace r2render
 {
@@ -14,31 +14,6 @@ namespace r2render
 
 namespace p2048table
 {
-	class TextureTableBase
-	{
-	private:
-		using TextureValueT = std::unique_ptr<r2render::Texture>;
-		using TextureContainerT = std::unordered_map<std::string, TextureValueT>;
-
-		using TextureFrameValueT = std::unique_ptr<r2render::TextureFrame>;
-		using TextureFrameContainerT = std::unordered_map<std::string, TextureFrameValueT>;
-
-	public:
-		TextureTableBase();
-
-		virtual void Load();
-
-		bool AddTexture( const char* const key_name, const uint32_t width, const uint32_t height, const char fill_char );
-		bool RemoveTexture( const char* const key_name );
-
-		r2render::Texture* const GetTexture( const char* const key_name ) const;
-		r2render::TextureFrame* const GetTextureFrame( const char* const key_name ) const;
-
-	private:
-		TextureContainerT mTextureContainer;
-		TextureFrameContainerT mTextureFrameContainer;
-	};
-
-	class TextureTable : public TextureTableBase, public r2::SingleTon<TextureTableBase>
+	class TextureTable : public r2base::TextureTableBase, public r2::SingleTon<TextureTable>
 	{};
 }
