2eee20076a6f3afec18867dc82823b11be158636
r2road <studior2road@gmail.com>
2022-03-21 20:26:33 +0900

    ...

--- r2bix/src/r2/r2_Random.h
+++ r2bix/src/r2/r2_Random.h
@@ -7,7 +7,9 @@ namespace r2
 	public:
 		static int GetInt( const int min, const int max );
 		static int GetInt_0To1();
+
 		static float GetFloat( const float min, const float max );
+
 		static bool GetBool();
 	};
 }
