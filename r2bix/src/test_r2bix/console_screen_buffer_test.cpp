946828f58663d47e0966011303846452b3bec0c1
r2road <studior2road@gmail.com>
2022-03-23 10:01:52 +0900

    ...

--- r2bix/src/test_r2bix/console_screen_buffer_test.cpp
+++ r2bix/src/test_r2bix/console_screen_buffer_test.cpp
@@ -3,6 +3,7 @@
 
 #include <Windows.h>
 
+#include "r2/r2_Inspector.h"
 #include "r2cm/r2cm_eTestEndAction.h"
 
 namespace console_screen_buffer_test
@@ -19,58 +20,45 @@ namespace console_screen_buffer_test
 		return []()->r2cm::eTestEndAction
 		{
 			std::cout << "# " << GetInstance().GetTitleFunction()() << " #" << r2::linefeed2;
-			std::cout << "[ESC] End" << r2::linefeed;
 
 			std::cout << r2::split;
 
+			DECLARATION_MAIN( HANDLE hStdout = GetStdHandle( STD_OUTPUT_HANDLE ) );
+			DECLARATION_MAIN( CONSOLE_SCREEN_BUFFER_INFO cs_buffer_info );
+
+			std::cout << r2::split;
+
+			DECLARATION_MAIN( const auto result = GetConsoleScreenBufferInfo( hStdout, &cs_buffer_info ) );
+			EXPECT_TRUE( result );
+
+			std::cout << r2::split;
+
+			if( result )
 			{
-				HANDLE hStdout = GetStdHandle( STD_OUTPUT_HANDLE );
-				CONSOLE_SCREEN_BUFFER_INFO cs_buffer_info;
+				std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.dwCursorPosition" << r2::linefeed;
+				std::cout << r2::tab2 << "X : " << cs_buffer_info.dwCursorPosition.X << r2::linefeed;
+				std::cout << r2::tab2 << "Y : " << cs_buffer_info.dwCursorPosition.Y << r2::linefeed2;
 
-				{
-					std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
-					std::cout << r2::tab2 << "HANDLE hStdout = GetStdHandle( STD_OUTPUT_HANDLE );" << r2::linefeed;
-					std::cout << r2::tab2 << "CONSOLE_SCREEN_BUFFER_INFO cs_buffer_info;" << r2::linefeed;
-				}
+				std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.dwMaximumWindowSize" << r2::linefeed;
+				std::cout << r2::tab2 << "X : " << cs_buffer_info.dwMaximumWindowSize.X << r2::linefeed;
+				std::cout << r2::tab2 << "Y : " << cs_buffer_info.dwMaximumWindowSize.Y << r2::linefeed2;
 
-				std::cout << r2::split;
+				std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.dwSize" << r2::linefeed;
+				std::cout << r2::tab2 << "X : " << cs_buffer_info.dwSize.X << r2::linefeed;
+				std::cout << r2::tab2 << "Y : " << cs_buffer_info.dwSize.Y << r2::linefeed2;
 
-				const auto result = GetConsoleScreenBufferInfo( hStdout, &cs_buffer_info );
+				std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.srWindow" << r2::linefeed;
+				std::cout << r2::tab2 << "Left : " << cs_buffer_info.srWindow.Left << r2::linefeed;
+				std::cout << r2::tab2 << "Top : " << cs_buffer_info.srWindow.Top << r2::linefeed;
+				std::cout << r2::tab2 << "Right : " << cs_buffer_info.srWindow.Right << r2::linefeed2;
+				std::cout << r2::tab2 << "Bottom : " << cs_buffer_info.srWindow.Bottom << r2::linefeed;
 
-				{
-					std::cout << r2::tab << "+ Process" << r2::linefeed2;
-					std::cout << r2::tab2 << "GetConsoleScreenBufferInfo( hStdout, &cs_buffer_info )" << r2::linefeed;
-				}
-
-				std::cout << r2::split;
-
-				if( result )
-				{
-					std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.dwCursorPosition" << r2::linefeed;
-					std::cout << r2::tab2 << "X : " << cs_buffer_info.dwCursorPosition.X << r2::linefeed;
-					std::cout << r2::tab2 << "Y : " << cs_buffer_info.dwCursorPosition.Y << r2::linefeed2;
-
-					std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.dwMaximumWindowSize" << r2::linefeed;
-					std::cout << r2::tab2 << "X : " << cs_buffer_info.dwMaximumWindowSize.X << r2::linefeed;
-					std::cout << r2::tab2 << "Y : " << cs_buffer_info.dwMaximumWindowSize.Y << r2::linefeed2;
-
-					std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.dwSize" << r2::linefeed;
-					std::cout << r2::tab2 << "X : " << cs_buffer_info.dwSize.X << r2::linefeed;
-					std::cout << r2::tab2 << "Y : " << cs_buffer_info.dwSize.Y << r2::linefeed2;
-
-					std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.srWindow" << r2::linefeed;
-					std::cout << r2::tab2 << "Top : " << cs_buffer_info.srWindow.Top << r2::linefeed;
-					std::cout << r2::tab2 << "Bottom : " << cs_buffer_info.srWindow.Bottom << r2::linefeed;
-					std::cout << r2::tab2 << "Left : " << cs_buffer_info.srWindow.Left << r2::linefeed;
-					std::cout << r2::tab2 << "Right : " << cs_buffer_info.srWindow.Right << r2::linefeed2;
-
-					std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.wAttributes" << r2::linefeed;
-					std::cout << r2::tab2 << cs_buffer_info.wAttributes << r2::linefeed;
-				}
-				else
-				{
-					std::cout << r2::tab << "Failed : " "GetConsoleScreenBufferInfo( hStdout, &cs_buffer_info )" << r2::linefeed;
-				}
+				std::cout << r2::tab << "+ CONSOLE_SCREEN_BUFFER_INFO.wAttributes" << r2::linefeed;
+				std::cout << r2::tab2 << cs_buffer_info.wAttributes << r2::linefeed;
+			}
+			else
+			{
+				std::cout << r2::tab << "Failed : " "GetConsoleScreenBufferInfo( hStdout, &cs_buffer_info )" << r2::linefeed;
 			}
 
 			std::cout << r2::split;
