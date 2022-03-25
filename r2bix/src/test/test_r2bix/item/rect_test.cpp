59d8a8189ae97f74fd181aa1f91029e9bcafbd42
r2road <studior2road@gmail.com>
2022-03-25 11:06:38 +0900

    ...

--- r2bix/src/test/test_r2bix/item/rect_test.cpp
+++ r2bix/src/test/test_r2bix/item/rect_test.cpp
@@ -5,6 +5,7 @@
 #include <conio.h>
 #include <Windows.h>
 
+#include "r2/r2_Inspector.h"
 #include "r2/r2_Rect_Int.h"
 #include "r2cm/r2cm_eTestEndAction.h"
 #include "test/Utility4Test.h"
@@ -26,23 +27,15 @@ namespace rect_test
 
 			std::cout << r2::split;
 
-			const r2::RectInt rect_0( 0, 0, 1, 1 );
-			const r2::RectInt rect_1( 4, 4, 6, 6 );
-			const r2::RectInt rect_2( 6, 6, 6, 6 );
-			const auto rect_3 = rect_1.IntersectsWithRect( rect_2 );
-
-			{
-				std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
-				std::cout << r2::tab2 << "const r2::RectInt rect_0( 0, 0, 1, 1 );" << r2::linefeed;
-				std::cout << r2::tab2 << "const r2::RectInt rect_1( 4, 4, 6, 6 );" << r2::linefeed;
-				std::cout << r2::tab2 << "const r2::RectInt rect_2( 6, 6, 6, 6 );" << r2::linefeed;
-				std::cout << r2::tab2 << "const auto rect_3 = rect_1.IntersectsWithRect( rect_2 );" << r2::linefeed;
-			}
+			DECLARATION_MAIN( const r2::RectInt rect_0( 0, 0, 1, 1 ) );
+			DECLARATION_MAIN( const r2::RectInt rect_1( 4, 4, 6, 6 ) );
+			DECLARATION_MAIN( const r2::RectInt rect_2( 6, 6, 6, 6 ) );
+			DECLARATION_MAIN( const auto rect_3 = rect_1.IntersectsWithRect( rect_2 ) );
 
 			std::cout << r2::split;
 
 			{
-				const int offset_y = 13;
+				const int offset_y = 11;
 
 				Utility4Test::DrawRect( offset_y, rect_0, '0' );
 				Utility4Test::DrawRect( offset_y, rect_1, '1' );
