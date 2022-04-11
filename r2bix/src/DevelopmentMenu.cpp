a45a89017adc91a3cf35e16f25af28dd9f959f74
r2road <studior2road@gmail.com>
2022-04-11 22:42:07 +0900

    extract

---
+++ r2bix/src/DevelopmentMenu.cpp
@@ -0,0 +1,118 @@
+#include "pch.h"
+#include "DevelopmentMenu.h"
+
+#include "base/r2base_Director.h"
+#include "r2cm/r2cm_Director.h"
+#include "r2cm/r2cm_eTestEndAction.h"
+
+#include "test/TestMainMenu.h"
+#include "p2048/p2048_EntryScene.h"
+#include "p2048mini/p2048mini_GameScene.h"
+#include "pmr/pmr_CompanyScene.h"
+
+r2cm::MenuUp DevelopmentMenu::Create( r2cm::Director& director )
+{
+	r2cm::MenuUp ret( new ( std::nothrow ) r2cm::Menu(
+		director
+		, GetTitle(),
+				"> Inprogress : ..."
+		"\n"	"> To do : ..."
+	) );
+
+	{
+		ret->AddItem(
+			'1'
+			, []()->const char* { return "Test"; }
+			, []()->r2cm::eTestEndAction
+			{
+				// 2022.04.11 by R2Road
+				// 인자로 넘어온 director 를 사용해도 되지만 아래 코드와의 일관성을 위해 새 r2cm::Director를 만들어 돌린다.
+
+				//
+				// Setup
+				//
+				r2cm::Director director;
+				director.Setup( TestMainMenu::Create( director ) );
+
+				//
+				// Process
+				//
+				director.Run();
+
+				return r2cm::eTestEndAction::None;
+			}
+		);
+		ret->AddItem(
+			'2'
+			, []()->const char* { return p2048mini::GameScene::GetTitle(); }
+			, []()->r2cm::eTestEndAction
+			{
+				//
+				// Setup
+				//
+				r2base::Director director;
+				director.Setup( p2048mini::GameScene::Create( director ) );
+
+				//
+				// Process
+				//
+				director.Run();
+
+				return r2cm::eTestEndAction::None;
+			}
+		);
+		ret->AddItem(
+			'3'
+			, []()->const char* { return p2048::EntryScene::GetTitle(); }
+			, []()->r2cm::eTestEndAction
+			{
+				//
+				// Setup
+				//
+				r2base::Director director;
+				director.Setup( p2048::EntryScene::Create( director ) );
+
+				//
+				// Process
+				//
+				director.Run();
+
+				return r2cm::eTestEndAction::None;
+			}
+		);
+		ret->AddItem(
+			'4'
+			, []()->const char* { return pmr::CompanyScene::GetTitle(); }
+			, []()->r2cm::eTestEndAction
+			{
+				//
+				// Setup
+				//
+				r2base::Director director;
+				director.Setup( pmr::CompanyScene::Create( director ) );
+
+				//
+				// Process
+				//
+				director.Run();
+
+				return r2cm::eTestEndAction::None;
+			}
+		);
+
+
+		ret->AddSplit();
+
+
+		ret->AddItem(
+			27
+			, []()->const char* { return "Exit"; }
+			, []()->r2cm::eTestEndAction
+			{
+				return r2cm::eTestEndAction::Exit;
+			}
+		);
+	}
+
+	return ret;
+}
--- r2bix/src/main.cpp
+++ r2bix/src/main.cpp
@@ -1,20 +1,13 @@
 ﻿#include "pch.h"
 
-#include <conio.h>
-
 #if defined( DEBUG ) || defined( _DEBUG )
 	#include <vld.h>
 #endif
 
-#include "base/r2base_Director.h"
-#include "p2048/p2048_EntryScene.h"
-#include "p2048mini/p2048mini_GameScene.h"
-#include "pmr/pmr_CompanyScene.h"
-
 #include "r2cm/r2cm_Director.h"
-#include "test/TestMainMenu.h"
-
 #include "utility/r2utility_WindowUtil.h"
+
+#include "DevelopmentMenu.h"
 
 int main()
 {
@@ -33,96 +26,16 @@ int main()
 	//
 	r2utility::MoveWindow( 0, 0 );
 
-	bool bRun = true;
-	do
-	{
-		system( "cls" );
+	//
+	// Setup
+	//
+	r2cm::Director director;
+	director.Setup( DevelopmentMenu::Create( director ) );
 
-		std::cout << "# Develop #" << r2::linefeed;
-
-		std::cout << r2::split;
-
-		std::cout << "+ Menu" << r2::linefeed2;
-
-		std::cout << "[ESC] " "Exit" << r2::linefeed2;
-
-		std::cout << "[1] " "Test" << r2::linefeed;
-		std::cout << "[2] " << p2048mini::GameScene::GetTitle() << r2::linefeed;
-		std::cout << "[3] " << p2048::EntryScene::GetTitle() << r2::linefeed;
-		std::cout << "[4] " << pmr::CompanyScene::GetTitle() << r2::linefeed;
-
-		std::cout << r2::split;
-
-		std::cout << "Select Menu";
-
-		const auto input = _getch();
-		switch( input )
-		{
-		case '1':
-		{
-			//
-			// Setup
-			//
-			r2cm::Director director;
-			director.Setup( TestMainMenu::Create( director ) );
-
-			//
-			// Process
-			//
-			director.Run();
-		}
-		break;
-
-		case '2':
-		{
-			//
-			// Setup
-			//
-			r2base::Director director;
-			director.Setup( p2048mini::GameScene::Create( director ) );
-
-			//
-			// Process
-			//
-			director.Run();
-		}
-		break;
-		case '3':
-		{
-			//
-			// Setup
-			//
-			r2base::Director director;
-			director.Setup( p2048::EntryScene::Create( director ) );
-
-			//
-			// Process
-			//
-			director.Run();
-		}
-		break;
-
-		case '4':
-		{
-			//
-			// Setup
-			//
-			r2base::Director director;
-			director.Setup( pmr::CompanyScene::Create( director ) );
-
-			//
-			// Process
-			//
-			director.Run();
-		}
-		break;
-
-		case 27:
-			bRun = false;
-			break;
-		}
-
-	} while( bRun );
+	//
+	// Process
+	//
+	director.Run();
 
 	return 0;
 }
