81bf66abdcbbd45ec0e6737c7e213a029dab6eb7
r2road <studior2road@gmail.com>
2022-04-27 21:22:08 +0900

    ...

--- r2bix/src/test/test_r2bix/item/action_test.h
+++ r2bix/src/test/test_r2bix/item/action_test.h
@@ -11,42 +11,49 @@ namespace action_test
 		TitleFuncT GetTitleFunction() const override;
 		DoFuncT GetDoFunction() override;
 	};
+
 	class DelayActionTest : public r2cm::iItem, public r2::SingleTon<DelayActionTest>
 	{
 	public:
 		TitleFuncT GetTitleFunction() const override;
 		DoFuncT GetDoFunction() override;
 	};
+
 	class SequenceActionTest : public r2cm::iItem, public r2::SingleTon<SequenceActionTest>
 	{
 	public:
 		TitleFuncT GetTitleFunction() const override;
 		DoFuncT GetDoFunction() override;
 	};
+
 	class MoveByActionTest : public r2cm::iItem, public r2::SingleTon<MoveByActionTest>
 	{
 	public:
 		TitleFuncT GetTitleFunction() const override;
 		DoFuncT GetDoFunction() override;
 	};
+
 	class RepeatActionTest : public r2cm::iItem, public r2::SingleTon<RepeatActionTest>
 	{
 	public:
 		TitleFuncT GetTitleFunction() const override;
 		DoFuncT GetDoFunction() override;
 	};
+
 	class BlinkActionTest : public r2cm::iItem, public r2::SingleTon<BlinkActionTest>
 	{
 	public:
 		TitleFuncT GetTitleFunction() const override;
 		DoFuncT GetDoFunction() override;
 	};
+
 	class CallbackActionTest : public r2cm::iItem, public r2::SingleTon<CallbackActionTest>
 	{
 	public:
 		TitleFuncT GetTitleFunction() const override;
 		DoFuncT GetDoFunction() override;
 	};
+
 	class AnimationRequestActionTest : public r2cm::iItem, public r2::SingleTon<AnimationRequestActionTest>
 	{
 	public:
