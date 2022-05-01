ece89e5f1b2b5e4c395779032d6fd8f2f1eea543
r2road <studior2road@gmail.com>
2022-05-02 01:42:05 +0900

    ...

--- r2bix/src/DevelopmentMenu.cpp
+++ r2bix/src/DevelopmentMenu.cpp
@@ -15,7 +15,7 @@ r2cm::MenuUp DevelopmentMenu::Create( r2cm::Director& director )
 	r2cm::MenuUp ret( new ( std::nothrow ) r2cm::Menu(
 		director
 		, GetTitle(),
-				"> Inprogress : p2048mini"
+				"> Inprogress : p2048"
 		"\n"	"> To do : Texture Editor 가 필요하다."
 	) );
 
