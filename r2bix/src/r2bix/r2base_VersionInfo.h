079565eede2c3e63d791308c8e1ddc30d593009d
r2road <studior2road@gmail.com>
2022-05-06 13:04:34 +0900

    check

--- r2bix/src/r2bix/r2base_VersionInfo.h
+++ r2bix/src/r2bix/r2base_VersionInfo.h
@@ -9,13 +9,13 @@ namespace r2base
 		const char VersionNumber_3 = 1;
 
 		const char* const String =
-			"### Version 0.0.1 ###"				"\n"
-			"[ ] " "Double Buffering"			"\n"
-			"[ ] " "Input"						"\n"
-			"[ ] " "Render Pipe Line"			"\n"
-			"[ ] " "Scene Node"					"\n"
-			"[ ] " "Component"					"\n"
-			"[ ] " "Make Simple Puzzle Game"
+			"### Version 0.0.1 ###"				"\n\n"
+			"[o] " "Double Buffering"			"\n"
+			"[o] " "Input"						"\n"
+			"[o] " "Render Pipe Line"			"\n"
+			"[o] " "Scene Node"					"\n"
+			"[o] " "Component"					"\n"
+			"[o] " "Make Simple Puzzle Game"
 		;
 	} VersionInfo;
 }
