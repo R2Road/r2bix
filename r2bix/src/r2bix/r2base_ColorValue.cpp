f74da555b2ffdbc3f4e0a8672fd7e20ba66ba913
r2road <studior2road@gmail.com>
2022-06-17 08:21:55 +0900

    ...

--- r2bix/src/r2bix/r2base_ColorValue.cpp
+++ r2bix/src/r2bix/r2base_ColorValue.cpp
@@ -5,12 +5,12 @@ namespace r2base
 {
 	unsigned char GetForegroundColor( const ColorValue color_value )
 	{
-		static const uint8_t mask = static_cast<uint8_t>( -1 ) >> 4;
+		static const uint8_t mask = ( static_cast<uint8_t>( -1 ) >> 4 );
 		return color_value & mask;
 	}
 	unsigned char GetBackgroundColor( const ColorValue color_value )
 	{
-		static const uint8_t mask = ( static_cast<uint8_t>( -1 ) >> 4 ) << 4;
+		static const uint8_t mask = ( ( static_cast<uint8_t>( -1 ) >> 4 ) << 4 );
 		return color_value & mask;
 	}
 }
