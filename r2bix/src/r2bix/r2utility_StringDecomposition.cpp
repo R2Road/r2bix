1c0748e702c70d5f9509b960116ff2ef296efcd8
r2road <studior2road@gmail.com>
2022-07-21 21:40:16 +0900

    miss... o_o;;;

---
+++ r2bix/src/r2bix/r2utility_StringDecomposition.cpp
@@ -0,0 +1,42 @@
+#include "r2utility_StringDecomposition.h"
+
+namespace r2utility
+{
+	StringDecomposition::ResultContainerT StringDecomposition::Do( const std::string_view str )
+	{
+		ResultContainerT out_container;
+
+		if( !str.empty() )
+		{
+			const char* key_string = "\n";
+
+			std::size_t pivot_pos = 0u;
+			std::size_t find_pos = 0u;
+			std::size_t fixed_find_pos = 0u;
+
+			do
+			{
+
+				find_pos = str.find( key_string, pivot_pos );
+				if( std::string::npos == find_pos )
+				{
+					fixed_find_pos = str.size();
+				}
+				else
+				{
+					fixed_find_pos = find_pos;
+				}
+
+				//
+				//
+				//
+				out_container.push_back( str.substr( pivot_pos, fixed_find_pos - pivot_pos ) );
+
+				pivot_pos = fixed_find_pos + 1u;
+
+			} while( std::string::npos != find_pos && pivot_pos <= str.size() );
+		}
+
+		return out_container;
+	}
+}
