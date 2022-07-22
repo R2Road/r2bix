d287e1673a4ab32e33a98ed6a8d9485256e885b3
r2road <studior2road@gmail.com>
2022-07-23 00:53:45 +0900

    ...

--- r2bix/src/test/test_r2bix/menu/TextureMenu.cpp
+++ r2bix/src/test/test_r2bix/menu/TextureMenu.cpp
@@ -3,11 +3,11 @@
 #include "r2cm/r2cm_Director.h"
 #include "r2cm/r2cm_constant.h"
 
-#include "test/test_r2bix/R2bixMenu.h"
-
 #include "test/test_r2bix/item/texture_test.h"
 #include "test/test_r2bix/item/texture_frame_test.h"
 #include "test/test_r2bix/item/texture_table_test.h"
+
+#include "test/test_r2bix/R2bixMenu.h"
 
 r2cm::MenuUp TextureMenu::Create( r2cm::Director& director )
 {
@@ -58,15 +58,7 @@ r2cm::MenuUp TextureMenu::Create( r2cm::Director& director )
 		ret->AddSplit();
 
 
-		ret->AddItem(
-			27
-			, []()->const char* { return "Return To Root"; }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-			director.Setup( R2bixMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+		ret->AddMenu<R2bixMenu>( 27 );
 	}
 
 	return ret;
