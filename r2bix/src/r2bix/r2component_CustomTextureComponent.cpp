3ee6d4df122e1f6412bb2c25c6ce59b26173d350
r2road <studior2road@gmail.com>
2022-08-17 22:00:03 +0900

    cleanup

--- r2bix/src/r2bix/r2component_CustomTextureComponent.cpp
+++ r2bix/src/r2bix/r2component_CustomTextureComponent.cpp
@@ -1,10 +1,5 @@
 #include "r2component_CustomTextureComponent.h"
 
-#include "r2base_DebugConfig.h"
-#include "r2base_Node.h"
-#include "r2component_TransformComponent.h"
-#include "r2render_Camera.h"
-
 namespace r2component
 {
 	CustomTextureComponent::CustomTextureComponent( r2base::Node& owner_node ) : r2base::Component<CustomTextureComponent>( owner_node )
