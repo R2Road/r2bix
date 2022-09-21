d5a0f70bab11617895945d24772b153482e4dfe2
R2Road <studior2road@gmail.com>
2022-09-21 15:03:30 +0900

    add enum : r2bix_director - Config - eScheduleType

--- r2bix/src/r2bix/r2bix_director_Config.h
+++ r2bix/src/r2bix/r2bix_director_Config.h
@@ -11,5 +11,12 @@ namespace r2bix_director
 		r2::SizeInt ScreenBufferSIze = { 107, 53 };
 		const int UpdateFramePerSeconds = 30u;
 		const int RenderFramePerSeconds = 30u;
+
+		enum class eScheduleType
+		{
+			Infinite,
+			Sleep,
+		};
+		const eScheduleType ScheduleType = eScheduleType::Infinite;
 	};
 }
