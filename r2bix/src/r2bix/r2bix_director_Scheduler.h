d4c83b0159b388af3e65e2429dfe175204e83961
R2Road <studior2road@gmail.com>
2022-09-21 15:32:54 +0900

    using

--- r2bix/src/r2bix/r2bix_director_Scheduler.h
+++ r2bix/src/r2bix/r2bix_director_Scheduler.h
@@ -8,12 +8,15 @@ namespace r2bix_director
 
 	class Scheduler
 	{
+	private:
+		using ProcessorT = std::function<void()>;
+
 	public:
 		Scheduler( const Config& config, const std::function<void(float)> scene_update_func, const std::function<void()> scene_render_func );
 
 		void Do();
 
 	private:
-		std::function<void()> mProcessor;
+		ProcessorT mProcessor;
 	};
 }
