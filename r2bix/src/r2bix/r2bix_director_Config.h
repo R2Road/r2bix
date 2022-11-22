6c575eafefc91c678b8508a499ca9dc89538fd47
R2Road <studior2road@gmail.com>
2022-11-22 10:29:39 +0900

    apply stdint

--- r2bix/src/r2bix/r2bix_director_Config.h
+++ r2bix/src/r2bix/r2bix_director_Config.h
@@ -6,8 +6,8 @@ namespace r2bix_director
 {
 	struct Config
 	{
-		const int ScreenBufferSize_Width = 107;
-		const int ScreenBufferSize_Height = 53;
+		const int32_t ScreenBufferSize_Width = 107;
+		const int32_t ScreenBufferSize_Height = 53;
 
 		enum class eScheduleType
 		{
@@ -16,8 +16,8 @@ namespace r2bix_director
 		};
 		const eScheduleType ScheduleType = eScheduleType::Infinite;
 
-		const int UpdateFramePerSeconds = 30u;
-		const int RenderFramePerSeconds = 30u;
+		const int32_t UpdateFramePerSeconds = 30u;
+		const int32_t RenderFramePerSeconds = 30u;
 
 	};
 }
