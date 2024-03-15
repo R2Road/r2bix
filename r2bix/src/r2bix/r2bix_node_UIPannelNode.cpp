7543c67f838b325c2a7c2ec7ca9fcadd8fee4723
R2Road <studior2road@gmail.com>
2024-03-15 14:02:50 +0900

    ...

--- r2bix/src/r2bix/r2bix_node_UIPannelNode.cpp
+++ r2bix/src/r2bix/r2bix_node_UIPannelNode.cpp
@@ -12,14 +12,23 @@ namespace r2bix_node
 		auto ret( r2bix_node::Node::Create( director ) );
 		if( ret )
 		{
+
+			//
+			// 생성
+			//
 			auto custome_texture_component = ret->AddComponent<r2bix_component::CustomTextureComponent>();
 			auto texture_render_component = ret->AddComponent<r2bix_component::TextureRenderComponent>();
 			auto ui_pannel_component = ret->AddComponent<r2bix_component::UIPannelComponent>();
 
+
+			//
+			// 설정
+			//
 			texture_render_component->SetTexture( custome_texture_component->GetTexture() );
 
 			ui_pannel_component->SetCustomTextureComponent( custome_texture_component );
 			ui_pannel_component->SetTextureRenderComponent( texture_render_component );
+
 		}
 
 		return ret;
