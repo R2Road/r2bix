975b424bce1997e025137b5dc62521e30f308374
R2Road <studior2road@gmail.com>
2024-04-25 10:54:45 +0900

    add method

--- r2bix/src/r2bix/r2bix_director_ScreenBufferManager.cpp
+++ r2bix/src/r2bix/r2bix_director_ScreenBufferManager.cpp
@@ -102,11 +102,7 @@ namespace r2bix_director
 		//
 		// Rollback
 		//
-		if( INVALID_HANDLE_VALUE != mBufferHandleOriginal )
-		{
-			SetConsoleActiveScreenBuffer( mBufferHandleOriginal );
-			std::cout.rdbuf( mCoutOriginalStreamBuffer );
-		}
+		ShowOriginalBuffer();
 
 		//
 		// Close Handle List
@@ -205,5 +201,18 @@ namespace r2bix_director
 		//
 		SetConsoleActiveScreenBuffer( mBufferHandleList[mCurrentBufferIndex] );
 		std::cout.rdbuf( &mCoutBufferRedirectorList[mCurrentBufferIndex] );
+	}
+
+	void ScreenBufferManager::ShowOriginalBuffer()
+	{
+		if( INVALID_HANDLE_VALUE != mBufferHandleOriginal )
+		{
+			SetConsoleActiveScreenBuffer( mBufferHandleOriginal );
+			std::cout.rdbuf( mCoutOriginalStreamBuffer );
+		}
+	}
+	void ScreenBufferManager::CloseOriginalBuffer()
+	{
+		Swap();
 	}
 }
--- r2bix/src/r2bix/r2bix_director_ScreenBufferManager.h
+++ r2bix/src/r2bix/r2bix_director_ScreenBufferManager.h
@@ -33,6 +33,9 @@ namespace r2bix_director
 		void Write2BackBuffer( const r2bix_render::Texture* const texture );
 		void Swap();
 
+		void ShowOriginalBuffer();
+		void CloseOriginalBuffer();
+
 	private:
 		HandleT mBufferHandleOriginal;
 		std::streambuf* mCoutOriginalStreamBuffer;
