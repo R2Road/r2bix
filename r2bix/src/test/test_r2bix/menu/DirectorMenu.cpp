caf515dee8250749d2307f5d3f0039f213394417
R2Road <studior2road@gmail.com>
2024-04-25 13:22:52 +0900

    add test : Director Test Scene

--- r2bix/src/test/test_r2bix/menu/DirectorMenu.cpp
+++ r2bix/src/test/test_r2bix/menu/DirectorMenu.cpp
@@ -8,6 +8,13 @@
 #include "../item/screen_buffer_manager_test.h"
 #include "../item/director_scheduler_test.h"
 
+#include "r2bix/r2bix_Director.h"
+#include "r2bix/r2bix_component_CustomTextureComponent.h"
+#include "r2bix/r2bix_component_InputComponent.h"
+#include "r2bix/r2bix_component_TextureRenderComponent.h"
+#include "r2bix/r2bix_node_CustomTextureNode.h"
+#include "r2bix/r2bix_utility_InputUtil.h"
+
 r2tm::TitleFunctionT DirectorMenu::GetTitleFunction() const
 {
 	return []()->const char*
@@ -36,6 +43,62 @@ r2tm::WriteFunctionT DirectorMenu::GetWriteFunction() const
 
 
 		ret->AddItem( 'q', director_scheduler_test::Declaration() );
+
+
+		ret->AddLineFeed();
+		ret->AddLineFeed();
+
+
+		ret->AddItem(
+			  32
+			, []()->const char* { return "Director Test Scene"; }
+			, []()->r2tm::eDoLeaveAction
+		{
+			r2bix::Director director( { 51, 51, r2bix_director::Config::eScheduleType::Sleep, 30, 60, 2, 1 } );
+
+			//
+			// Scene
+			//
+			r2bix_node::Node* root_node;
+			{
+				auto temp = r2bix_node::Node::Create( director );
+				root_node = temp.get();
+
+				director.Setup( std::move( temp ) );
+
+				auto input_component = root_node->AddComponent<r2bix_component::InputComponent>();
+				input_component->SetKeyboardCallback( r2bix_input::eKeyCode::VK_ESCAPE, [&director]( r2bix_input::eKeyStatus )->bool
+				{
+					director.RequestAbort();
+					r2bix_utility::ClearCInputBuffer();
+
+					return true;
+				} );
+
+				input_component->Activate();
+			}
+
+			//
+			// Background
+			//
+			{
+				auto node = root_node->AddChild<r2bix_node::CustomTextureNode>();
+				node->GetComponent<r2bix_component::CustomTextureComponent>()->GetTexture()->Reset( 50, 50, '#', r2bix::ColorValue( r2bix::eBackgroundColor::BG_Gray ) );
+				node->GetComponent<r2bix_component::TextureRenderComponent>()->SetPivotPoint( 0.f, 0.f );
+			}
+
+			//
+			// Process
+			//
+			director.Run();
+
+			//
+			// Terminate
+			//
+			director.Terminate();
+
+			return r2tm::eDoLeaveAction::None;
+		} );
 
 
 		ret->AddSplit();
