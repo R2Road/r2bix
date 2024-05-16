2f96bbb3d94b184d67a0dc02e3066e1772eaac34
R2Road <studior2road@gmail.com>
2024-05-16 14:57:50 +0900

    is not Node

--- r2bix/src/p2048/p2048_CompanyScene.cpp
+++ r2bix/src/p2048/p2048_CompanyScene.cpp
@@ -1,101 +1,134 @@
 #include "p2048_CompanyScene.h"
 
-#include <cassert>
-#include <conio.h>
-#include <utility> // std::move
+#include "r2_TimerFloat.h"
 
 #include "r2bix_Director.h"
+
+#include "r2bix_component_InputKeyboardComponent.h"
 #include "r2bix_component_LabelSComponent.h"
 #include "r2bix_component_TextureFrameAnimationComponent.h"
 #include "r2bix_component_TextureFrameRenderComponent.h"
 #include "r2bix_component_TextureRenderComponent.h"
 #include "r2bix_component_TransformComponent.h"
+
 #include "r2bix_node_LabelSNode.h"
 #include "r2bix_node_SpriteAnimationNode.h"
+
+#include "r2bix_utility_InputUtil.h"
 
 #include "p2048_TitleScene.h"
 #include "p2048table_TextureFrameAnimationTable.h"
 #include "p2048table_TextureTable.h"
 
+
 namespace p2048
 {
-	CompanyScene::CompanyScene( r2bix::Director& director ) : r2bix_node::Node( director )
-		, mChangeSceneTimer( 2.f, true )
-	{}
-
-	r2bix_node::NodeUp CompanyScene::Create( r2bix::Director& director )
+	class CompanySceneComponent : public r2bix_component::Component<CompanySceneComponent>
 	{
-		r2bix_node::NodeUp ret( new ( std::nothrow ) CompanyScene( director ) );
-		if( !ret->Init() )
-		{
-			assert( false );
-		}
+	public:
+		CompanySceneComponent( r2bix_node::Node& owner_node ) : r2bix_component::Component<CompanySceneComponent>( owner_node )
+			, mChangeSceneTimer( 2.f, true )
+		{}
 
-		return ret;
-	}
-
-	bool CompanyScene::Init()
-	{
-		if( !r2bix_node::Node::Init() )
-		{
-			return false;
-		}
 
 		//
-		// Company Name
+		// Override
 		//
+		void Update( const float delta_time ) override
 		{
-			auto label_node = AddChild<r2bix_node::LabelSNode>();
-
-			label_node->GetComponent<r2bix_component::LabelSComponent>()->SetString( "R2Road Studio" );
-
-			label_node->mTransformComponent->SetPosition(
-				mDirector.GetScreenSize().GetWidth() * 0.5f
-				, mDirector.GetScreenSize().GetHeight() * 0.62f
-			);
-		}
-
-		//
-		// Logo
-		//
-		{
-			auto sprite_animation_node = AddChild<r2bix_node::SpriteAnimationNode>( 1 );
-
-			sprite_animation_node->GetComponent<r2bix_component::TextureFrameAnimationComponent>()->LoadAnimation(
-				p2048table::TextureFrameAnimationTable::GetInstance().Get( 1 )
-			);
-			sprite_animation_node->GetComponent<r2bix_component::TextureFrameAnimationComponent>()->RunAnimation_Repeat( r2bix_animation::eIndex::Run_1 );
-
-			sprite_animation_node->mTransformComponent->SetPosition(
-				mDirector.GetScreenSize().GetWidth() * 0.5f
-				, mDirector.GetScreenSize().GetHeight() * 0.42f
-			);
-		}
-
-		return true;
-	}
-	void CompanyScene::Update( const float delta_time )
-	{
-		if( !mChangeSceneTimer.Update( delta_time ) )
-		{
-			mDirector.Setup( p2048::TitleScene::Create( mDirector ) );
-		}
-		else
-		{
-			if( _kbhit() )
+			if( !mChangeSceneTimer.Update( delta_time ) )
 			{
-				auto input = _getch();
-				if( 27 == input ) // ESC
-				{
-					mDirector.RequestAbort();
-				}
-				else if( 32 == input ) // Space
-				{
-					mDirector.Setup( p2048::TitleScene::Create( mDirector ) );
-				}
+				GetOwnerNode().GetDirector().Setup( p2048::TitleScene::Create( GetOwnerNode().GetDirector() ) );
 			}
 		}
 
-		r2bix_node::Node::Update( delta_time );
+
+	private:
+		r2::TimerFloat mChangeSceneTimer;
+	};
+
+
+
+	r2bix_node::NodeUp CompanyScene::Create( r2bix::Director& director )
+	{
+		r2bix_node::NodeUp ret( r2bix_node::Node::Create( director ) );
+		if( ret )
+		{
+			//
+			// Company Name
+			//
+			{
+				auto label_node = ret->AddChild<r2bix_node::LabelSNode>();
+
+				label_node->GetComponent<r2bix_component::LabelSComponent>()->SetString( "R2Road Studio" );
+
+				label_node->mTransformComponent->SetPosition(
+					  director.GetScreenSize().GetWidth() * 0.5f
+					, director.GetScreenSize().GetHeight() * 0.62f
+				);
+			}
+
+			//
+			// Logo
+			//
+			{
+				auto sprite_animation_node = ret->AddChild<r2bix_node::SpriteAnimationNode>( 1 );
+
+				sprite_animation_node->GetComponent<r2bix_component::TextureFrameAnimationComponent>()->LoadAnimation(
+					p2048table::TextureFrameAnimationTable::GetInstance().Get( 1 )
+				);
+				sprite_animation_node->GetComponent<r2bix_component::TextureFrameAnimationComponent>()->RunAnimation_Repeat( r2bix_animation::eIndex::Run_1 );
+
+				sprite_animation_node->mTransformComponent->SetPosition(
+					  director.GetScreenSize().GetWidth() * 0.5f
+					, director.GetScreenSize().GetHeight() * 0.42f
+				);
+			}
+
+			//
+			// Input
+			//
+			{
+				auto component = ret->AddComponent<r2bix_component::InputKeyboardComponent>();
+
+				//
+				// Exit
+				//
+				component->SetCallback(
+					  r2bix_input::eKeyCode::VK_ESCAPE
+					, [&director]( r2bix_input::eKeyStatus s )->bool
+					{
+						if( r2bix_input::eKeyStatus::Push == s )
+						{
+							r2bix_utility::ClearCInputBuffer();
+							director.RequestAbort();
+							return true;
+						}
+
+						return false;
+					}
+				);
+
+				//
+				// Move 2 Title Scene
+				//
+				component->SetCallback(
+					  r2bix_input::eKeyCode::VK_SPACE
+					, [&director]( r2bix_input::eKeyStatus s )->bool
+					{
+						if( r2bix_input::eKeyStatus::Push == s )
+						{
+							director.Setup( p2048::TitleScene::Create( director ) );
+							return true;
+						}
+
+						return false;
+					}
+				);
+				component->Activate();
+			}
+		}
+
+		return ret;
 	}
 }
--- r2bix/src/p2048/p2048_CompanyScene.h
+++ r2bix/src/p2048/p2048_CompanyScene.h
@@ -2,24 +2,14 @@
 
 #include "r2bix_node_Node.h"
 
-#include "r2_TimerFloat.h"
-
 namespace p2048
 {
-	class CompanyScene : public r2bix_node::Node
+	class CompanyScene
 	{
 	private:
-		CompanyScene( r2bix::Director& director );
+		CompanyScene() = delete;
 
 	public:
 		static r2bix_node::NodeUp Create( r2bix::Director& director );
-
-	private:
-		bool Init() override;
-	public:
-		void Update( const float delta_time ) override;
-
-	private:
-		r2::TimerFloat mChangeSceneTimer;
 	};
 }
