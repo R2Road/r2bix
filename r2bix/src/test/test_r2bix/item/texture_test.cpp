d758d5bc659e94d56a6724b6e8d9fc765bab260d
R2Road <studior2road@gmail.com>
2024-05-16 14:31:42 +0900

    move

--- r2bix/src/test/test_r2bix/item/texture_test.cpp
+++ r2bix/src/test/test_r2bix/item/texture_test.cpp
@@ -5,7 +5,7 @@
 #include "r2bix_render_Texture.h"
 #include "r2bix_render_TextureFrame.h"
 #include "r2bix_helper/r2bix_helper_Printer4Texture.h"
-#include "r2bix_helper_STDPrinter4Texture.h"
+#include "r2bix_helper/r2bix_helper_STDPrinter4Texture.h"
 
 #include "r2tm/r2tm_Inspector.h"
 #include "r2tm/r2tm_ostream.h"
