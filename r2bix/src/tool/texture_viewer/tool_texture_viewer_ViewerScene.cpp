f7b94db04238594a0324926954f8844c4010d4dd
R2Road <studior2road@gmail.com>
2024-05-16 11:54:29 +0900

    update scene : exit with esc key

--- r2bix/src/tool/texture_viewer/tool_texture_viewer_ViewerScene.cpp
+++ r2bix/src/tool/texture_viewer/tool_texture_viewer_ViewerScene.cpp
@@ -1,10 +1,13 @@
 #include "tool_texture_viewer_ViewerScene.h"
 
 #include "r2bix_Director.h"
-#include "r2bix_node_Node.h"
 
+#include "r2bix_component_InputKeyboardComponent.h"
 #include "r2bix_component_LabelSComponent.h"
+
 #include "r2bix_node_LabelSNode.h"
+
+#include "r2bix_utility_InputUtil.h"
 
 namespace tool_texture_viewer
 {
@@ -13,13 +16,40 @@ namespace tool_texture_viewer
 		auto ret( r2bix_node::Node::Create( director ) );
 		if( ret )
 		{
-			auto node = ret->AddChild<r2bix_node::LabelSNode>();
-			node->GetComponent<r2bix_component::LabelSComponent>()->SetString( "Texture Viewer" );
+			//
+			// Exit
+			//
+			{
+				auto component = ret->AddComponent<r2bix_component::InputKeyboardComponent>();
+				component->SetCallback(
+					r2bix_input::eKeyCode::VK_ESCAPE
+					, [&director]( r2bix_input::eKeyStatus s )->bool
+					{
+						if( r2bix_input::eKeyStatus::Release == s )
+						{
+							r2bix_utility::ClearCInputBuffer();
+							director.RequestAbort();
+							return true;
+						}
 
-			node->mTransformComponent->SetPosition(
-				director.GetScreenSize().GetWidth() * 0.5f
-				, director.GetScreenSize().GetHeight() * 0.5f
-			);
+						return false;
+					}
+				);
+				component->Activate();
+			}
+
+			//
+			//
+			//
+			{
+				auto node = ret->AddChild<r2bix_node::LabelSNode>();
+				node->GetComponent<r2bix_component::LabelSComponent>()->SetString( "Texture Viewer" );
+
+				node->mTransformComponent->SetPosition(
+					  director.GetScreenSize().GetWidth() * 0.5f
+					, director.GetScreenSize().GetHeight() * 0.5f
+				);
+			}
 		}
 
 		return ret;
