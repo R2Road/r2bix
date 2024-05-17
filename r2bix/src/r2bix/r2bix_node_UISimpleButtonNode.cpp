d620b6a72cdecacbe64f3a56c88c6dd4d5e63d41
R2Road <studior2road@gmail.com>
2024-05-17 15:41:12 +0900

    add class : r2bix_node - UISimpleButtonNode

---
+++ r2bix/src/r2bix/r2bix_node_UISimpleButtonNode.cpp
@@ -0,0 +1,47 @@
+#include "r2bix_node_UISimpleButtonNode.h"
+
+#include "r2bix_Director.h"
+
+#include "r2bix_component_UIButtonComponent.h"
+#include "r2bix_component_UIControlComponent.h"
+
+namespace r2bix_node
+{
+	r2bix_node::NodeUp UISimpleButtonNode::Create( r2bix::Director& director )
+	{
+		auto ret( r2bix_node::Node::Create( director ) );
+		if( ret )
+		{
+
+			//
+			// 생성
+			//
+			auto ui_button_component = ret->AddComponent<r2bix_component::UIButtonComponent>();
+			auto ui_control_component = ret->AddComponent<r2bix_component::UIControlComponent>();
+
+
+
+			//
+			// 설정
+			//
+			ui_button_component->SetMyUIControlComponent( ui_control_component );
+
+
+
+			//
+			// 활성화
+			//
+			if( true )
+			{
+				ui_button_component->Activate();
+			}
+			if( true )
+			{
+				ui_control_component->Activate();
+			}
+
+		}
+
+		return ret;
+	}
+}
---
+++ r2bix/src/r2bix/r2bix_node_UISimpleButtonNode.h
@@ -0,0 +1,26 @@
+//
+// # 특별한 디자인이 요구되지 않는 단순한 버튼
+//  > CustomTexture Component 를 View로 사용
+//  > 색상 변경 만으로 버튼 상태에 대응
+//
+
+#pragma once
+
+#include "r2bix_node_Node.h"
+
+namespace r2bix
+{
+	class Director;
+}
+
+namespace r2bix_node
+{
+	class UISimpleButtonNode : public r2bix_node::Node
+	{
+	private:
+		UISimpleButtonNode() = delete;
+
+	public:
+		static r2bix_node::NodeUp Create( r2bix::Director& director );
+	};
+}
