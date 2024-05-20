399ea3862144b07713c2cf92d10e81c353a7e960
R2Road <studior2road@gmail.com>
2024-05-20 11:04:58 +0900

    ...

--- r2bix/src/r2bix/r2bix_render_TextureFrame.h
+++ r2bix/src/r2bix/r2bix_render_TextureFrame.h
@@ -9,23 +9,51 @@ namespace r2bix_render
 	public:
 		explicit TextureFrame( const Texture* const texture );
 
+
+
 		//
 		//
 		//
-		const Texture* const GetTexture() const { return mTexture; }
+		const Texture* const GetTexture() const
+		{
+			return mTexture;
+		}
 
-		int GetWidth() const { return mVisibleRect.GetWidth(); }
-		int GetHeight() const { return mVisibleRect.GetHeight(); }
+		int GetWidth() const
+		{
+			return mVisibleRect.GetWidth();
+		}
+		int GetHeight() const
+		{
+			return mVisibleRect.GetHeight();
+		}
 
-		int GetMinX() const { return mVisibleRect.GetMinX(); }
-		int GetMinY() const { return mVisibleRect.GetMinY(); }
-		int GetMaxX() const { return mVisibleRect.GetMaxX(); }
-		int GetMaxY() const { return mVisibleRect.GetMaxY(); }
+		int GetMinX() const
+		{
+			return mVisibleRect.GetMinX();
+		}
+		int GetMinY() const
+		{
+			return mVisibleRect.GetMinY();
+		}
+		int GetMaxX() const
+		{
+			return mVisibleRect.GetMaxX();
+		}
+		int GetMaxY() const
+		{
+			return mVisibleRect.GetMaxY();
+		}
 
-		const r2::RectInt& GetVisibleRect() const { return mVisibleRect; }
+		const r2::RectInt& GetVisibleRect() const
+		{
+			return mVisibleRect;
+		}
 		Texture::ValueT GetCharacter( const uint32_t x, const uint32_t y ) const;
 		r2bix::ColorValue GetColor( const uint32_t x, const uint32_t y ) const;
 		bool GetCharacterDisuse( const uint32_t x, const uint32_t y ) const;
+
+
 
 		//
 		//
@@ -35,6 +63,8 @@ namespace r2bix_render
 		void MoveVisibleOrigin( const int mx, const int my ) { mVisibleRect.MoveOrigin( mx, my ); }
 		void MoveVisibleOrigin( const r2::RectInt::PointT& point ) { mVisibleRect.MoveOrigin( point.GetX(), point.GetY() ); }
 		void ChangeVisibleSize( const int change_width, const int change_height ) { mVisibleRect.ChangeSize( change_width, change_height ); }
+
+
 
 	private:
 		const Texture* const mTexture;
