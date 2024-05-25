e50a7ab32a1aa53f03878c4063ac214829eeb2dd
R2Road <studior2road@gmail.com>
2024-05-25 14:19:05 +0900

    fill x 2

--- r2bix/src/tool/texture_editor/tool_texture_editor_EditorComponent.cpp
+++ r2bix/src/tool/texture_editor/tool_texture_editor_EditorComponent.cpp
@@ -12,25 +12,43 @@ namespace tool_texture_editor
 {
 	EditorComponent::EditorComponent( r2bix::Director& director, r2bix_node::Node& owner_node ) : r2bix_component::Component<EditorComponent>( director, owner_node )
 		, mFileName()
-		, mSlot4KeyResponse( [this]( int, r2bix_ui::eKeyStatus )->bool
+		, mSlot4CursorResponse( [this]( r2bix_ui::eCursorStatus s )
 		{
-			//
-			// Do Something
-			//
+			switch( s )
+			{
+			case r2bix_ui::eCursorStatus::CursorOver:
+			case r2bix_ui::eCursorStatus::CursorMove:
+				if( mbFill )
+				{
 
-			auto texture_node = mCanvasNode->GetChildByName( "texture" );
+					auto texture_node = mCanvasNode->GetChildByName( "texture" );
 
-			auto fill_point = GetDirector().GetInputManager().GetCursorPoint();
+					auto fill_point = GetDirector().GetInputManager().GetCursorPoint();
 
-			auto texture_world_position = texture_node->mTransformComponent->GetWorldPosition();
+					auto texture_world_position = texture_node->mTransformComponent->GetWorldPosition();
 
-			fill_point.Minus( texture_world_position.GetX(), texture_world_position.GetY() );
-			
-			texture_node->GetComponent<r2bix_component::CustomTextureComponent>()->GetTexture()->FillCharacter( fill_point.GetX(), fill_point.GetY(), '+' );
+					fill_point.Minus( texture_world_position.GetX(), texture_world_position.GetY() );
+
+					texture_node->GetComponent<r2bix_component::CustomTextureComponent>()->GetTexture()->FillCharacter( fill_point.GetX(), fill_point.GetY(), '+' );
+				}
+				break;
+			}
+		} )
+		, mSlot4KeyResponse( [this]( int, const r2bix_ui::eKeyStatus s )->bool
+		{
+			if( s == r2bix_ui::eKeyStatus::Push || s == r2bix_ui::eKeyStatus::Pressed )
+			{
+				mbFill = true;
+			}
+			else
+			{
+				mbFill = false;
+			}
 
 			return true;
 		} )
 		, mCanvasNode( nullptr )
+		, mbFill( false )
 	{}
 
 
@@ -51,6 +69,7 @@ namespace tool_texture_editor
 	void EditorComponent::SetCanvas( r2bix_node::Node* canvas_node )
 	{
 		mCanvasNode = canvas_node;
+		mCanvasNode->GetComponent<r2bix_component::UIControlComponent>()->ConnectSlot4CursorResponse( &mSlot4CursorResponse );
 		mCanvasNode->GetComponent<r2bix_component::UIControlComponent>()->ConnectSlot4KeyResponse( &mSlot4KeyResponse );
 	}
 }
--- r2bix/src/tool/texture_editor/tool_texture_editor_EditorComponent.h
+++ r2bix/src/tool/texture_editor/tool_texture_editor_EditorComponent.h
@@ -13,6 +13,7 @@ namespace tool_texture_editor
 	class EditorComponent : public r2bix_component::Component<EditorComponent>
 	{
 	public:
+		using Slot4CursorResponseT = r2::Slot<void, r2bix_ui::eCursorStatus>;
 		using Slot4KeyResponseT = r2::Slot<bool, int, r2bix_ui::eKeyStatus>;
 
 
@@ -37,8 +38,10 @@ namespace tool_texture_editor
 
 	private:
 		std::string mFileName;
+		Slot4CursorResponseT mSlot4CursorResponse;
 		Slot4KeyResponseT mSlot4KeyResponse;
 
 		r2bix_node::Node* mCanvasNode;
+		bool mbFill;
 	};
 }
