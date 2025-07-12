0447093013ac9cac4760ce8e2132ca3d91ef1045
R2Road <studior2road@gmail.com>
2025-07-12 16:39:54 +0900

    add doc

---
+++ doc/3D_파이프라인.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1752298137652" ID="ID_1283915968" MODIFIED="1752298505682" TEXT="3D &#xb80c;&#xb354;&#xb9c1; &#xd30c;&#xc774;&#xd504; &#xb77c;&#xc778; &#xad6c;&#xcd95;">
+<node CREATED="1752298421726" HGAP="60" ID="ID_1086530340" MODIFIED="1752298484096" POSITION="right" TEXT="&#xae30;&#xc874;&#xc758; 2D &#xc804;&#xc6a9; &#xc2dc;&#xc2a4;&#xd15c;&#xc740; &#xd544;&#xc694; &#xc5c6;&#xb2e4;." VSHIFT="-61"/>
+<node CREATED="1752298514638" HGAP="45" ID="ID_1451239968" MODIFIED="1752298528579" POSITION="right" TEXT="&#xc791;&#xc5c5; &#xacfc;&#xc815;" VSHIFT="-50">
+<node CREATED="1752298445598" HGAP="18" ID="ID_82871159" MODIFIED="1752298556687" TEXT="Static Mesh &#xac1d;&#xccb4;&#xb97c; &#xb3c4;&#xc785;&#xd558;&#xace0; 2D &#xc2dc;&#xc2a4;&#xd15c;&#xc5d0; &#xc801;&#xc6a9;" VSHIFT="-2"/>
+<node CREATED="1752298533526" ID="ID_5778796" MODIFIED="1752298553730" TEXT="&#xae30;&#xc874; &#xce74;&#xba54;&#xb77c;&#xb97c; 3D &#xce74;&#xba54;&#xb77c;&#xb85c; &#xbcc0;&#xd658;"/>
+</node>
+<node CREATED="1752305876389" ID="ID_1068613217" MODIFIED="1752305880292" POSITION="left" TEXT="&#xce74;&#xba54;&#xb77c;">
+<node CREATED="1752305883539" ID="ID_311447494" MODIFIED="1752305892859" TEXT="2d &#xce74;&#xba54;&#xb77c;&#xc640; 3d &#xce74;&#xba54;&#xb77c;&#xc758; &#xad6c;&#xbd84;&#xc774; &#xd544;&#xc694;"/>
+</node>
+</node>
+</map>
