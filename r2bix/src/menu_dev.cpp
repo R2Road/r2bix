fd81363f09df65793e6c2190dec0c143cf73e8ad
R2Road <studior2road@gmail.com>
2025-08-10 01:02:50 +0900

    cleanup : 앞으로 프로젝트는 별도의 프로젝트로 만들 것

--- r2bix/src/menu_dev.cpp
+++ r2bix/src/menu_dev.cpp
@@ -8,15 +8,10 @@
 #include "r2tm/r2tm_MenuProcessor.h"
 #include "r2tm/r2tm_VersionInfo.h"
 
-#include "test/project_a/ProjectAMenu.h"
 #include "test/test_p2048/P2048Menu.h"
-#include "test/test_psnake/PSnakeMenu.h"
 #include "test/test_r2bix/menu_r2bix.h"
 
 #include "tool/ToolMenu.h"
-
-#include "pmr/pmr_CompanyScene.h"
-#include "project_mini_adventure/p_mini_adv_CompanyScene.h"
 
 #include "view/menu_view.h"
 
@@ -59,73 +54,13 @@ r2tm::WriteFunctionT Menu_Dev::GetWriteFunction() const
 
 
 		mp->AddLineFeed();
-		mp->AddLineFeed();
-
-
-
-		mp->AddMenu( 'q', ProjectAMenu() );
-
-
-
-		mp->AddLineFeed();
-		mp->AddLineFeed();
 
 		
 
-		mp->AddMenu( 'a', PSnakeMenu() );
-		mp->AddMenu( 's', P2048Menu() );
-		mp->AddItem(
-			'd'
-			, []()->const char* { return p_mini_adv::CompanyScene::GetTitle(); }
-			, []()->r2tm::eDoLeaveAction
-			{
-				//
-				// Setup
-				//
-				r2bix::Director director( {} );
-				director.Setup( p_mini_adv::CompanyScene::Create( director ) );
-
-				//
-				// Process
-				//
-				director.Run();
-
-				//
-				// Terminate
-				//
-				director.Terminate();
-
-				return r2tm::eDoLeaveAction::None;
-			}
-		);
-		mp->AddItem(
-			'f'
-			, []()->const char* { return pmr::CompanyScene::GetTitle(); }
-			, []()->r2tm::eDoLeaveAction
-			{
-				//
-				// Setup
-				//
-				r2bix::Director director( {} );
-				director.Setup( pmr::CompanyScene::Create( director ) );
-
-				//
-				// Process
-				//
-				director.Run();
-
-				//
-				// Terminate
-				//
-				director.Terminate();
-
-				return r2tm::eDoLeaveAction::None;
-			}
-		);
+		mp->AddMenu( 'a', P2048Menu() );
 
 
 
-		mp->AddLineFeed();
 		mp->AddLineFeed();
 
 
