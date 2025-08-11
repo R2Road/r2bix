4e9bb253962a8b91c8eac193ab9d36f5950c03a3
R2Road <studior2road@gmail.com>
2025-08-11 16:31:36 +0900

    ...

--- project/src/menu_dev.cpp
+++ project/src/menu_dev.cpp
@@ -73,13 +73,6 @@ r2tm::WriteFunctionT Menu_Dev::GetWriteFunction() const
 
 
 
-		mp->AddItem(
-			27
-			, []()->const char* { return "Exit"; }
-			, []()->r2tm::eDoLeaveAction
-			{
-				return r2tm::eDoLeaveAction::Exit;
-			}
-		);
+		mp->AddExit( 27 );
 	};
 }
